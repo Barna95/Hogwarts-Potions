2ba5e74307deb318e4c14a8735d61941ee56f797
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-16 16:44:26 +0100

    create : roomservice

---
+++ Data/Services/IRoomService.cs
@@ -0,0 +1,16 @@
+﻿using HogwartsPotions.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Data.Services
+{
+    public interface IRoomService
+    {
+        public Task AddRoom(Room room);
+        public Task<Room> GetRoom(long roomId);
+        public Task<List<Room>> GetAllRooms();
+        public Task UpdateRoom(long id, Room room);
+        public Task DeleteRoom(long id);
+        public Task<List<Room>> GetRoomsForRatOwners();
+    }
+}
---
+++ Data/Services/RoomService.cs
@@ -0,0 +1,61 @@
+﻿using HogwartsPotions.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using HogwartsPotions.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HogwartsPotions.Data.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly HogwartsContext _context;
+        public RoomService(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddRoom(Room room)
+        { 
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Room> GetRoom(long roomId)
+        {
+            return await _context.Rooms.FirstOrDefaultAsync(r => r.ID == roomId);
+        }
+
+        public async Task<List<Room>> GetAllRooms()
+        {
+            return await _context.Rooms.Include(r => r.Residents).ToListAsync();
+        }
+
+        public async Task UpdateRoom(long id, Room room)
+        {
+            var updateRoom = _context.Rooms.FirstOrDefault(r => r.ID == id);
+            updateRoom.Capacity = room.Capacity;
+            updateRoom.Residents = room.Residents;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoom(long id)
+        {
+            var deleteRoom = await _context.Rooms.FirstOrDefaultAsync(r => r.ID == id);
+            _context.Rooms.Remove(deleteRoom);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Room>> GetRoomsForRatOwners()
+        {
+            return await _context.Rooms
+                .Include(r => r.Residents)
+                .Where(room => !room.Residents
+                    .Any(resident =>
+                        resident.PetType == PetType.Cat ||
+                        resident.PetType == PetType.Owl))
+                .ToListAsync();
+        }
+    }
+}
