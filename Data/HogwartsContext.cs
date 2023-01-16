a3004f670462c5ce27670e38f1afbb400742ad18
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-16 16:40:04 +0100

    update : HogwartsContext

---
+++ Data/HogwartsContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Data
+{
+    public class HogwartsContext : DbContext
+    {
+        public const int MaxIngredientsForPotions = 5;
+
+        public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
