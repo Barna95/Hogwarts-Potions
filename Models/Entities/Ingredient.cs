bcb58cf8f0c0ee722325d1f9c1861ba455a5a523
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-17 12:15:06 +0100

    create : Ingredient

---
+++ Models/Entities/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Ingredient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+        public string Name { get; set; }
+    }
+}
