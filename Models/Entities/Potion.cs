830cd49b3e1cf22a5aacf7a95ccb2ef7a4a57da8
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-17 18:28:58 +0100

    create : potion

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Potion
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+        public Student Student { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public Recipe Recipe { get; set; }
+        public BrewingStatus BrewingStatus { get; set; }
+    }
+}
