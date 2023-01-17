e9f7a8c76d3df41d29aae865abbddb46108a3ae8
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-17 18:34:24 +0100

    update : seed

--- Data/DbInit.cs
+++ Data/DbInit.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using HogwartsPotions.Models.Entities;
@@ -16,7 +15,7 @@ namespace HogwartsPotions.Data
             {
                 var context = serviceScope.ServiceProvider.GetService<HogwartsContext>();
                 context.Database.EnsureCreated();
-
+    //Rooms -----------------------------------------------
                 var room1 = new Room()
                 {
                     Capacity = 4,
@@ -40,41 +39,98 @@ namespace HogwartsPotions.Data
                     Capacity = 4,
                     Residents = new HashSet<Student>()
                 };
+    // Students --------------------------------------------------
+                var Jóska = new Student()
+                {
+                    Name = "Jóska",
+                    HouseType = HouseType.Gryffindor,
+                    PetType = PetType.Rat,
+                    Room = null
+                };
+                var Pista = new Student()
+                {
+                    Name = "Pista",
+                    HouseType = HouseType.Slytherin,
+                    PetType = PetType.None,
+                    Room = null
+                };
+                var Mari = new Student()
+                {
+                    Name = "Mari",
+                    HouseType = HouseType.Ravenclaw,
+                    PetType = PetType.Owl,
+                    Room = null
+                };
+                var Maris = new Student()
+                {
+                    Name = "Maris",
+                    HouseType = HouseType.Hufflepuff,
+                    PetType = PetType.Rat,
+                    Room = null
+                };
+                room1.Residents.Add(Pista);
+                room2.Residents.Add(Mari);
+                room2.Residents.Add(Maris);
+                room1.Residents.Add(Jóska);
+    //Ingredients ---------------------------------------------------
+                var ginger = new Ingredient()
+                {
+                    Name = "Ginger"
+                };
+                var catFur = new Ingredient()
+                {
+                    Name = "CatFur"
+                };
+                var cannabis = new Ingredient()
+                {
+                    Name = "Cannabis"
+                };
+                var doomWeed = new Ingredient()
+                {
+                    Name = "DoomWeed"
+                };
+                var branchOfLife = new Ingredient()
+                {
+                    Name = "BranchOfLife"
+                };
+                var dragonScale = new Ingredient()
+                {
+                    Name = "DragonScale"
+                }; var paprika = new Ingredient()
+                {
+                    Name = "Paprika"
+                };
+    //Recipe ------------------------------------------------------
+                var jóskaRecipe = new Recipe()
+                {
+                    Name = "Jóska's discovery#1",
+                    student = Jóska,
+                    Ingredients = new List<Ingredient>(),
+                };
+                jóskaRecipe.Ingredients.Add(paprika);
+                jóskaRecipe.Ingredients.Add(branchOfLife);
+                jóskaRecipe.Ingredients.Add(dragonScale);
+                jóskaRecipe.Ingredients.Add(doomWeed);
+                jóskaRecipe.Ingredients.Add(ginger);
+
+                //Potion ---------------------------------------------------------
+                var jóskaPotion = new Potion()
+                {
+                    BrewingStatus = BrewingStatus.Discovery,
+                    Ingredients = new List<Ingredient>(),
+                    Name = "Jóska Potion",
+                    Recipe = jóskaRecipe,
+                    Student = Jóska
+                };
+                jóskaPotion.Ingredients.Add(paprika);
+                jóskaPotion.Ingredients.Add(branchOfLife);
+                jóskaPotion.Ingredients.Add(dragonScale);
+                jóskaPotion.Ingredients.Add(doomWeed);
+                jóskaPotion.Ingredients.Add(ginger);
 
                 if (!context.Students.Any())
                 {
-                    var Jóska = new Student()
-                    {
-                        Name = "Jóska",
-                        HouseType = HouseType.Gryffindor,
-                        PetType = PetType.Rat,
-                        Room = null
-                    };
-                    var Pista = new Student()
-                    {
-                        Name = "Pista",
-                        HouseType = HouseType.Slytherin,
-                        PetType = PetType.None,
-                        Room = null
-                    };
-                    var Mari = new Student()
-                    {
-                        Name = "Mari",
-                        HouseType = HouseType.Ravenclaw,
-                        PetType = PetType.Owl,
-                        Room = null
-                    };
-                    var Maris = new Student()
-                    {
-                        Name = "Maris",
-                        HouseType = HouseType.Hufflepuff,
-                        PetType = PetType.Rat,
-                        Room = null
-                    };
-                    room1.Residents.Add(Pista);
-                    room2.Residents.Add(Mari);
-                    room2.Residents.Add(Maris);
-                    room1.Residents.Add(Jóska);
+                    
                     context.Students.AddRange(new List<Student>()
                     {
                         Jóska,
@@ -96,8 +152,39 @@ namespace HogwartsPotions.Data
                     });
                     context.SaveChanges();
                 }
+                if (!context.Ingredients.Any())
+                {
+                    context.Ingredients.AddRange(new List<Ingredient>()
+                    {
+                        ginger,
+                        catFur,
+                        cannabis,
+                        doomWeed,
+                        branchOfLife,
+                        dragonScale,
+                        paprika,
 
-               
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Recipes.Any())
+                {
+                    context.Recipes.AddRange(new List<Recipe>()
+                    {
+                        jóskaRecipe
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Potions.Any())
+                {
+                    context.Potions.AddRange(new List<Potion>()
+                    {
+                        jóskaPotion
+                    });
+                    context.SaveChanges();
+                }
             }
         }
     }
